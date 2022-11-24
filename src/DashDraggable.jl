c6d4491e2a055169f4a528667c8309d450d5c33e
Léon Lazar <leon.lazar@mercedes-benz.com>
2022-11-24 16:18:44 +0100

    ✅ [BUILD] Added current build files with DashItemResponsive fix
    Signed-off-by: Léon Lazar <leon.lazar@mercedes-benz.com>

--- src/DashDraggable.jl
+++ src/DashDraggable.jl
@@ -3,7 +3,7 @@ module DashDraggable
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.1.2"
+const version = "0.1.3"
 
 include("''_dashboarditem.jl")
 include("''_dashboarditemresponsive.jl")
